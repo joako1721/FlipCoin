1bc44a82d09da1f5ce4afe807c33f9fe2ebac927
Sp <41648020+TheoJSP@users.noreply.github.com>
2020-10-13 17:57:22 -0300

    🥑

---
+++ app.js
@@ -0,0 +1,14 @@
+function flip(){
+    var text = document.getElementById("text").value;
+    var text1 = document.getElementById("text1").value;
+    var resultado = document.getElementById("resultado")
+    var random = Math.floor(Math.random() * 2)
+    console.log(random)
+    
+    if(random == 0){
+        resultado.value = text1
+    }else if(random == 1){
+        resultado.value = text
+    }
+    
+}
